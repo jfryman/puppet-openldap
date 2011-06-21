e6a3a8b16df931cf58ec63eb9a83131e27ef60e7
James Fryman <james@frymanet.com>
2011-06-21 10:08:04 -0700

    Base Module Commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: ldap
+#
+# This module manages ldap
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class ldap {
+
+
+}
